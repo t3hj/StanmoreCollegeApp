2e6d5c4049046f4d6c3c8d27367aa0b2a349ebc9
t3hj <tehjpatel@gmail.com>
2024-10-20 20:52:39 +0100

    Add files via upload

---
+++ Stanmore_App/Stanmore_App/Models/Foods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stanmore_App.Models
+{
+    public class Foods
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public float Price { get; set; }
+        public int Count { get; set; }
+
+    }
+}
---
+++ Stanmore_App/Stanmore_App/Models/Items.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stanmore_App.Models
+{
+    public class Items
+    {
+
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+
+    }
+}
